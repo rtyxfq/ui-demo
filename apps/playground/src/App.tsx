7392f34a80f7379bf0f135792625cec8e9e43a5a
rtyxfq <18567115867@163.com>
2025-11-27 16:03:41 +0800

    :new: 调用组件demo

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -5,31 +5,12 @@ import { Button } from '@ui-demo/ui';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Button />
+      <Button onClick={() => alert('Component from UI library clicked!')}>
+        Monorepo Button
+      </Button>
     </>
   )
 }
